e0cd70737b73150975f0a83dd3fd49cd236bf02b
fatraccoon <kashfia135@gmail.com>
2020-08-23 05:01:08 -0400

    Found index.js

---
+++ index.js
@@ -0,0 +1,44 @@
+// defining constants
+const express = require('express');
+const webpush = require('web-push');
+const bodyParser = require('body-parser');
+const path = require('path'); // module is core node.js module, defines static path for client
+
+const app = express();
+
+// store client data, set static path
+app.use(express.static(path.join(__dirname, "client")));
+
+// body-parser middleware
+app.use (bodyParser.json());
+
+// access web-push location to generate keys
+const publicVapidKey = 
+    'BJXyGxIMu9nR-pDCAswU8cxnj3UpWTGyEZsYV0Y5CsIBtmbHa8CLm7oCG4xYgNywjs9c2UqQs9_fP5Wed_DyVT4';
+const privateVapidKey = 'DT0U2C3J75Hub1MCe3BAvv8x9s1k2JatFXSOM3YC53U';
+
+// set VAPID keys using web-push, identify who is sending notification
+webpush.setVapidDetails(
+    'mailto:test@test.com', 
+    publicVapidKey, 
+    privateVapidKey); // test email
+
+// subscribe route
+app.post('/subscribe', (req, res) => {
+    // get pushSubscription object
+    const subscription = req.body;
+
+    // return a status - 201 = success
+    res.status(201).json({});
+
+    // create payload (optional, could be title of notif)
+    const payload = JSON.stringify({ title: 'Push Test' });
+    
+    // pass object into sendNotificaiton
+    webpush
+        .sendNotification(subscription, payload)
+        .catch(err => console.error(err));
+});
+
+const port = 3000;
+app.listen(port, () => console.log(`Server started on port ${port}`));
